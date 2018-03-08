c019f19c25c8aeb5644c120e77ae0ee13726b2c4
Husein Habibie <36624685+Husein-H@users.noreply.github.com>
2018-03-08 12:27:13 +0700

    Add files via upload

---
+++ Exercises 6.js
@@ -0,0 +1,17 @@
+console.log("Looping Pertama")
+var index = 1;
+while(index <=20)
+{
+  if(index % 2 === 0){
+    console.log(index + " i love coding")
+  }
+  index++;
+}
+console.log("Looping kedua")
+var index2 = 20;
+while(index2 >=1){
+  if(index2 % 2 === 0){
+    console.log(index2 + " I will become fullstack developer")
+  }
+  index2--;
+}
---
+++ Exercises 9.js
@@ -0,0 +1,29 @@
+function shoutout(){
+  
+  return 'halo function!'}
+  console.log(shoutout());
+  
+  ------------------------------------------------------------------------------------------------
+  
+  function CalculateMultiply(){
+  var num1=5;
+  var num2=6;
+  return num1*num2
+  
+}
+  var hasilperkalian = CalculateMultiply 
+  console.log(hasilperkalian());
+  
+  -------------------------------------------------------------------------------------------------
+  
+  function processSentence(){
+  return "nama saya " + name + ",umur saya " + age + " tahun, alamat saya di " + address + "dan saya punya hobby yaitu " + hobby
+}
+  var name= "habibie";
+  var age=21;
+  var address="jln.yos sudarso perum apt no 1b, Balikpapan ";
+  var hobby="singing, gaming, eating"
+  
+  
+  var fullSentence = processSentence (name,age,address,hobby) 
+  console.log(fullSentence)
